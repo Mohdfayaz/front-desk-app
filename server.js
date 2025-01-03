4be981c82137fda0a52c1de695a577ef6d93c03d
Mohammed Fayaz z <mohammedfayazfn173148@gmail.com>
2025-01-03 20:12:26 +0530

    'splitting'

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const { MONGODB_URI, PORT } = require("./utils/config");
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello World!' });
+app.get("/", (request, response) => {
+  response.json({ message: "Hello World!" });
 });
 
-app.listen(3001, () => {
-  console.log('Server is running @ http://localhost:3001');
+app.listen(PORT, () => {
+  console.log(`Server is running @ http://localhost:3001`);
 });
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => {
-  console.log("Connected to MongoDB");
-})
-    .catch((error) => {
-    console.log("Error connecting to MongoDB:", error);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB", error);
   });
